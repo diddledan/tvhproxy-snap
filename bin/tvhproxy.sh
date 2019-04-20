a1115cfdf05e82cdf4a4d0201e593f30892e4275
Daniel Llewellyn <daniel@bowlhat.net>
2019-04-20 18:22:27 +0100

    Update bin/tvhproxy.sh with more config options
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- bin/tvhproxy.sh
+++ bin/tvhproxy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+### TVHeadend URL
+
 TVH_URL=$(snapctl get tvh.url)
 if [ -z "$TVH_URL" ]; then
 	TVH_URL="http://localhost:9981"
@@ -7,4 +9,20 @@ fi
 
 export TVH_URL
 
-exec python3 $SNAP/tvhProxy.py
+### Proxy URL
+
+TVH_PROXY_URL=$(snapctl get tvh.proxy-url)
+[ -z "$TVH_PROXY_URL" ] && TVH_PROXY_URL="$(echo $TVH_URL | sed -E -e 's|^(https?://)([^/:]+)(:9981)?(/.*)$|http://\2:5004/|')"
+
+export TVH_PROXY_URL
+
+### Tuner Count
+
+TVH_TUNER_COUNT=$(snapctl get tvh.tuner-count)
+[ -z "$TVH_TUNER_COUNT" ] && TVH_TUNER_COUNT=1
+
+export TVH_TUNER_COUNT
+
+### Start the server
+
+exec python3 $SNAP/tvhProxy.py "$@"
